8a2b2d6428612d55a5edb4dec19972fdec9a34df
Marc Morera <yuhu@mmoreram.com>
2019-05-11 12:25:24 +0200

    Arguments are nor inside Class
    - Configured bootstrap and adapters

--- src/server.php
+++ src/server.php
@@ -14,27 +14,75 @@
  * @author Marc Morera <yuhu@mmoreram.com>
  */
 
+/**
+ * Make key value on arguments
+ *
+ * @param array $originalArguments
+ *
+ * @return array
+ *
+ * @throws Exception
+ */
+function buildServerArguments(array $originalArguments)
+{
+    $arguments = array_slice($originalArguments, 2);
+    $newArguments = [];
+    foreach ($arguments as $value) {
+        $parts = explode('=', $value, 2);
+        $key = $parts[0];
+        $value = $parts[1] ?? true;
+        $newArguments[$key] = $value;
+    }
+
+    $serverArgs = explode(':', $originalArguments[1], 2);
+    if (count($serverArgs) !== 2) {
+        throw new Exception('You should start the server defining a host and a port as a first argument: php/server 0.0.0.0:8000');
+    }
+
+    list($host, $port) = $serverArgs;
+    $newArguments['host'] = $host;
+    $newArguments['port'] = $port;
+
+    return $newArguments;
+}
+
+
 use Apisearch\ReactSymfonyServer\Adapter\KernelAdapter;
-use Apisearch\ReactSymfonyServer\Arguments;
+use Apisearch\ReactSymfonyServer\Adapter\Symfony4KernelAdapter;
 use Symfony\Component\Debug\Debug;
 
-require dirname(__FILE__).'/../config/bootstrap.php';
-
-$arguments = Arguments::build($argv);
+/**
+ * Server
+ */
+$arguments = buildServerArguments($argv);
 $environment = array_key_exists('--dev', $arguments) ? 'dev' : 'prod';
-$appPath = __DIR__.'/..';
+$appPath = dirname(__FILE__).'/..';
 $silent = $arguments['--silent'] ?? false;
 $debug = $arguments['--debug'] ?? false;
 $adapter = $arguments['--adapter'] ?? 'symfony4';
+$bootstrap = $arguments['--bootstrap'] ?? 'symfony4';
+var_dump($bootstrap);
 $host = $arguments['host'];
 $port = $arguments['port'];
 
+$bootstrapFile = [
+    'autoload' => $appPath . '/vendor/autoload.php',
+    'symfony4' => $appPath . '/config/bootstrap.php',
+][$bootstrap] ?? $bootstrap;
+
+$bootstrapFile = realpath($bootstrapFile);
+if (!is_file($bootstrapFile)) {
+    throw new \Exception('You must define an existing kernel bootstrap file, or by an alias or my a file path');
+}
+
+require realpath($bootstrapFile);
+
 $adapter = [
-    'symfony4' => \Apisearch\ReactSymfonyServer\Adapter\Symfony4KernelAdapter::class
+    'symfony4' => Symfony4KernelAdapter::class
 ][$adapter] ?? null;
 
-if (null === $adapter) {
-    throw new \Exception('You must define an existing kernel adapter, or by an alias (symfony4) or my a namespace');
+if (!is_a($adapter, Symfony4KernelAdapter::class, true)) {
+    throw new \Exception('You must define an existing kernel adapter, or by an alias or my a namespace. This class MUST implement KernelAdapter');
 }
 
 if (!$silent) {
@@ -50,6 +98,7 @@ if (!$silent) {
     echo ">  Silent: " . ($debug ? 'enabled' : 'disabled') . PHP_EOL;
     echo ">  Silent: disabled" . PHP_EOL;
     echo ">  Adapter: $adapter" . PHP_EOL;
+    echo ">  Bootstrap: $bootstrapFile" . PHP_EOL;
     echo '>' . PHP_EOL . PHP_EOL;
 }
 
