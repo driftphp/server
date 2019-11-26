b9e8360727f12c45def5173ca177aa3000894d0d
cv65kr <cv65kr@gmail.com>
2019-11-26 17:36:30 +0100

    Type hint for array_map

--- src/Console/WatchServerCommand.php
+++ src/Console/WatchServerCommand.php
@@ -132,12 +132,12 @@ final class WatchServerCommand extends ServerCommand
         array $folders,
         string $rootPath
     ): array {
-        $folders = array_map(function (string $path) use ($rootPath) {
+        $folders = array_map(function (string $path) use ($rootPath): ?string {
             $path = sprintf('%s/%s/', $rootPath, trim($path, '/'));
 
             return is_file($path) || is_dir($path)
                 ? $path
-                : false;
+                : null;
         }, $folders);
         $folders = array_filter($folders);
         $folders = array_map(function (string $folder) {
